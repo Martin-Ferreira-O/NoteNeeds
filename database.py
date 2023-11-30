ed3e7058f1889488e21da563419a1ffed990b33b
Martín Ferreira <martinferreiraolate@gmail.com>
2023-11-30 17:38:54 -0300

    Change: Database is a class now

--- database.py
+++ database.py
@@ -3,54 +3,53 @@ from pymongo.database import Database
 
 URI = ""
 
-def connect():
-    """Connect to MongoDB Atlas"""
-    client = MongoClient(URI)
+class Database:
+    def __init__(self, client: MongoClient):
+        self.client = client
+        self.db = client["users"]
 
-    return client["users"]
+    def create_profile(self, name, rut, university):
 
-def create_profile(client: Database, name, rut, university):
+        profile = self.db["profile"] # Select the "usuarios" collection from the database
+        _id = profile.count_documents({}) + 1
 
-    profile = client["profile"] # Select the "usuarios" collection from the database
-    _id = profile.count_documents({}) + 1
+        profile_default = {
+            "_id": _id,
+            "name": name,
+            "rut": rut,
+            "universidad": university
+        }
 
-    profile_default = {
-        "_id": _id,
-        "name": name,
-        "rut": rut,
-        "universidad": university
-    }
-
-    profile.insert_one(profile_default)
-
-
-def add_course(client: Database, course: dict):
-    # course need _id, name, grade[]
-    # grade: {grade, weight}
-    try:
-        client["course"].insert_one(course)
-    except Exception as e:
-        print(e)
+        profile.insert_one(profile_default)
 
 
-def add_grade(client: Database, course_id: int, grade: dict):
-    
-    try:
-        client["course"].find_one_and_update(
-            {"_id": course_id},
-            {"$push": {"grade": grade}}
-        )
-    except Exception as e:
-        print(e)
-
-def get_profile(client: Database, _id: int):
-    return client["profile"].find_one({"_id": _id})
-
-def get_course(client: Database, _id: int):
-    return client["course"].find_one({"_id": _id})
+    def add_course(self, course: dict):
+        # course need _id, name, grade[]
+        # grade: {grade, weight}
+        try:
+            self.db["course"].insert_one(course)
+        except Exception as e:
+            print(e)
 
 
-db = connect()
+    def add_grade(self, course_id: int, grade: dict):
+        
+        try:
+            self.db["course"].find_one_and_update(
+                {"_id": course_id},
+                {"$push": {"grade": grade}}
+            )
+        except Exception as e:
+            print(e)
+
+    def get_profile(self ,_id: int):
+        return self.db["profile"].find_one({"_id": _id})
+
+    def get_course(self, _id: int):
+        return self.db["course"].find_one({"_id": _id})
+
+
+#db = connect()
 #create_profile(db, "ALVARO", "123456789", "UA")
 #create_profile(db, "jaz", "123456789", "UA")
 
